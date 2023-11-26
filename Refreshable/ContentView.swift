587edaa2a3bd7ce4d7b47a41fc97cba3063a608e
Daniel Romero <dromero@usetandem.com>
2023-11-26 11:56:46 -0500

    Initial Commit

---
+++ Refreshable/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Refreshable
+//
+//  Created by Daniel Romero on 11/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Refreshable/RefreshableApp.swift
@@ -0,0 +1,17 @@
+//
+//  RefreshableApp.swift
+//  Refreshable
+//
+//  Created by Daniel Romero on 11/26/23.
+//
+
+import SwiftUI
+
+@main
+struct RefreshableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
